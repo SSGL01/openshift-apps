d4b9ab52614918c023f36fab5a7acecd3c4431ac
Soumi SG <ssg_learn@hotmail.com>
2026-06-14 17:55:02 +0530

    Updated

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-Create a GitHub repository
-Create a new file called index.php inside the repository with the following content - 
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming!\n";
