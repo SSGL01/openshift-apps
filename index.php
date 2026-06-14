f80259ca24d24e2fb726be689b4396399a46d535
Soumi SG <ssg_learn@hotmail.com>
2026-06-14 17:51:09 +0530

    Add initial PHP Script to print messages

---
+++ index.php
@@ -0,0 +1,6 @@
+Create a GitHub repository
+Create a new file called index.php inside the repository with the following content - 
+<?php
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "A change is coming!\n";
+?>
